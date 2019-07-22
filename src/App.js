599dc25cf51a8e113efa4dbdbb54c69a1a80f8d3
Stef <sjingo@users.noreply.github.com>
2019-07-22 21:55:41 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GMap from './views/Map'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+export function Home(){
+  return <>
+  <h2>Home</h2>
+  <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+        </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+        </a>
+        </>
+}
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Link to="/map/">Map</Link>
+      <Link to="/">Home</Link>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/Map/" component={GMap}></Route>
       </header>
     </div>
+    </Router>
   );
 }
 
